c11cd8c67295801b6b7a8b9ced2e8e34319d1753
Davee Sok <daveesok@gmail.com>
2021-04-27 00:11:06 -0500

    updating

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import Cards from './dbCards.js';
+
 // App Config
 const app = express();
 const port = process.env.PORT || 8001;
+const connection_url = 'mongodb+srv://tinderProjectAdmin:NcvCXb1Rt5rhZAlP@tinderclonecluster.zoc79.mongodb.net/tinderDB?retryWrites=true&w=majority';
 
 // Middlewares
 
 // DB config
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 // API Endpoints
 app.get('/', (req, res) => {
   res.status(200).send('Hello World');
+});
+
+app.post('/tinder/cards', (req, res) => {
+  const dbCard = req.body;
+
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get('/tinder/cards', (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
 });
 
 // Listener
